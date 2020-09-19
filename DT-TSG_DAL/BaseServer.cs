0d68b9d949da69d56ce160e92d99a052aec8de6d
bglb <bglb@qq.com>
2020-09-20 01:35:54 +0800

    新分支第一次提交

--- DT-TSG_DAL/BaseServer.cs
+++ DT-TSG_DAL/BaseServer.cs
@@ -12,14 +12,14 @@ namespace DTTSG_DAL
 {
     public class BaseServer<T> where T : class, new()
     {
-        
-        protected int Execute(string sql, object s = null)
+
+        protected int Execute(string sql, object param = null)
         {
             try
             {
                 using (IDbConnection connection = new SqlConnection(Config.connStr))
                 {
-                    int result = connection.Execute(sql,s);
+                    int result = connection.Execute(sql, param);
                 }
             }
             catch (Exception ex)
@@ -29,9 +29,54 @@ namespace DTTSG_DAL
             return 0;
         }
 
+        /// <summary>
+        /// 获取多条强类型数据
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <param name="param">sql参数</param>
+        /// <returns>强类型数据的List</returns>
+        protected List<T> GetList(string sql, object param = null)
+        {
+            try
+            {
+                using (IDbConnection connection = new SqlConnection(Config.connStr))
+                {
+                    return connection.Query<T>(sql, param).ToList();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //写入日志
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 返回单条强类型数据
+        /// </summary>
+        /// <param name="sql">sql</param>
+        /// <param name="param">sql参数</param>
+        /// <returns>强类型模型</returns>
+        protected T GetModel(string sql,object param=null)
+        {
+            try
+            {
+                using (IDbConnection connection = new SqlConnection(Config.connStr))
+                {
+                    return connection.Query<T>(sql, param) as T;
+                }
+            }
+            catch (Exception ex)
+            {
+                //写入日志
+            }
+            return null;
+        }
+
         public virtual Dictionary<string, object> InsertAssembleSql(T model)
         {
-            
+
             Dictionary<string, object> sqlobj = new Dictionary<string, object>();
             if (model != null)
             {
@@ -66,7 +111,7 @@ namespace DTTSG_DAL
 
                     }
 
-                }           
+                }
                 sql += (sqlName + sqlValues);
                 sqlobj.Add("sql", sql);
                 sqlobj.Add("parameters", parameters);
@@ -81,18 +126,18 @@ namespace DTTSG_DAL
 
         }
 
-        public virtual Dictionary<string,object> UpdateAssembleSql(T model)
+        public virtual Dictionary<string, object> UpdateAssembleSql(T model)
         {
             Dictionary<string, object> sqlobj = new Dictionary<string, object>();
             if (model != null)
             {
-                string sql = @"update " ;
+                string sql = @"update ";
                 string TableName = typeof(T).Name;// 表名称
                 Type type = model.GetType();
                 DynamicParameters parameters = new DynamicParameters();
                 sql += TableName;
                 string sqlSet = " set ";
-                
+
                 PropertyInfo[] propertyInfos = type.GetProperties();
                 var KEY = propertyInfos.Where(p => p.GetCustomAttributes(typeof(KeyAttribute), false).Length > 0).FirstOrDefault();
 
@@ -103,13 +148,13 @@ namespace DTTSG_DAL
                     string PropertyName = item.Name.ToString();
                     var PropertyValue = item.GetValue(model, null) ?? "";
                     if (!PropertyValue.ToString().StartsWith("DTTSG") && PropertyName != KEY.Name.ToString())// 排除外联表以及主键
-                    {                    
-                        sqlSet+= PropertyName + "=@"+PropertyName;
+                    {
+                        sqlSet += PropertyName + "=@" + PropertyName;
                         parameters.Add("@" + PropertyName, PropertyValue);
                         if (!propertyInfos.LastOrDefault().Equals(item))
                         {
                             sqlSet += ", ";
-                            
+
                         }
                         else
                         {
@@ -143,14 +188,14 @@ namespace DTTSG_DAL
                 Type type = model.GetType();
                 DynamicParameters parameters = new DynamicParameters();
                 sql += TableName;
-               
+
 
                 PropertyInfo[] propertyInfos = type.GetProperties();
                 var KEY = propertyInfos.Where(p => p.GetCustomAttributes(typeof(KeyAttribute), false).Length > 0).FirstOrDefault();
 
                 string sqlWhere = " where " + KEY.Name + "=@" + KEY.Name;
                 parameters.Add("@" + KEY.Name, KEY.GetValue(model));// 添加主键作为where选择条件
-                
+
                 sql += sqlWhere;
                 sqlobj.Add("sql", sql);
                 sqlobj.Add("parameters", parameters);
@@ -165,6 +210,7 @@ namespace DTTSG_DAL
 
         }
 
+       
     }
 }
 
