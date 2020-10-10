e231e64b2e7acdaa82c4a60bd658a9cea9a1daac
bglb <bglb@qq.com>
2020-10-10 17:10:44 +0800

    修复图片

---
+++ DT-TSG_DAL/Image/ImageServer.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using DTTSG_Model;
+
+namespace DTTSG_DAL
+{
+    public class ImageServer : BaseServer<ImageInfo>
+    {
+        public ImageInfo GetimageInfo(int imageId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@ImageId", imageId);
+
+            string sql = @"select * from ImageInfo where ImageId = @ImageId";
+            return GetModel(sql,parameters);
+        }
+    }
+}
